97e7ff097bd011d4f41eef35bb3dede264e836a9
YongChan Cho <h3236516@gmail.com>
2024-07-05 16:56:02 +0900

    remove: drizzle

--- src/drizzle/schema.ts
+++
@@ -1,8 +0,0 @@
-import { pgTable, text, serial } from 'drizzle-orm/pg-core';
-
-export const users = pgTable('users', {
-  id: serial('id').primaryKey(),
-  name: text('name'),
-  email: text('email').unique(),
-  password: text('password'),
-});
