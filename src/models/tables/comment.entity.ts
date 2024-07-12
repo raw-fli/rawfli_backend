0c8df57cc5929848e374399cad26b6638ebcd8ff
dp0973 <yeong0973@gmail.com>
2024-07-12 22:36:06 +0900

    feat: add entity schemas

---
+++ src/models/tables/comment.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import { CommonColumns } from '../common/common-columns';
+import { Post } from './post.entity';
+import { User } from './user.entity';
+
+@Entity()
+export class Comment extends CommonColumns {
+  @ManyToOne(() => Post, (post) => post.comments, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: "postId", referencedColumnName: "id" })
+  @JoinColumn({ name: "postBoardId", referencedColumnName: "board" })
+  post!: Post;
+
+  @ManyToOne(() => User, (user) => user.comments, { nullable: true, onDelete: 'SET NULL' })
+  author: User;
+
+  @Column({ type: 'text' })
+  content!: string;
+
+  @OneToMany(() => Comment, (comment) => comment.parent)
+  replies!: Comment[];
+
+  @ManyToOne(() => Comment, (comment) => comment.replies, { onDelete: 'CASCADE' })
+  parent!: Comment;
+}
---
+++ src/models/tables/photo.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { CommunityPost, GalleryPost } from './post.entity';
+import { User } from './user.entity';
+
+@Entity()
+export class Photo {
+  @PrimaryGeneratedColumn('uuid')
+  id!: number;
+
+  @ManyToOne(() => GalleryPost, (post) => post.photos, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: "postId", referencedColumnName: "id" })
+  @JoinColumn({ name: "postBoardId", referencedColumnName: "board" })
+  post!: GalleryPost;
+
+  @ManyToOne(() => User, (user) => user.photos, { nullable: true, onDelete: 'SET NULL' })
+  author: User;
+
+  @Column({ type: 'text' })
+  description!: string;
+
+  @ManyToMany(() => User, (user) => user.likedPhotos)
+  likes!: User[];
+
+  @ManyToMany(() => CommunityPost, (post) => post.referencedPhotos)
+  referencedInPosts!: CommunityPost[];
+}
