b740afa4c87a62bd0a797ff9f9616edad6bbc514
dp0973 <yeong0973@gmail.com>
2024-07-12 22:38:18 +0900

    style: errors consistency

--- src/config/errors/error.ts
+++ src/config/errors/error.ts
@@ -2,7 +2,7 @@
 
 import { ERROR } from '.';
 
-export interface ALREADY_CREATED_EMAIL extends ERROR {
+export interface EMAIL_ALREADY_CREATED extends ERROR {
   type: 'business';
   result: false;
   code: 4001;
@@ -16,44 +16,51 @@ export interface NO_AUTH_TOKEN extends ERROR {
   data: '인증이 필요합니다.';
 }
 
-export interface CANNOT_FINDONE_ARTICLE extends ERROR {
+export interface BOARD_NOT_FOUND extends ERROR {
   type: 'business';
   result: false;
   code: 4004;
-  data: '게시글을 찾지 못했습니다.';
+  data: '게시판을 찾지 못했어요.';
 }
 
-export interface NOT_FOUND_ARTICLE_TO_COMMENT extends ERROR {
+export interface ARTICLE_NOT_FOUND extends ERROR {
+  type: 'business';
+  result: false;
+  code: 4004;
+  data: '게시글을 찾지 못했어요.';
+}
+
+export interface COMMENT_NOT_FOUND extends ERROR {
+  type: 'business';
+  result: false;
+  code: 4016;
+  data: '댓글을 찾지 못했어요.';
+}
+
+export interface ARTICLE_TO_COMMENT_NOT_FOUND extends ERROR {
   type: 'business';
   result: false;
   code: 4006;
-  data: '댓글을 작성할 게시글을 찾지 못했습니다.';
+  data: '댓글을 작성할 게시글을 찾지 못했어요.';
 }
 
-export interface CANNOT_FIND_ONE_REPLY_COMMENT extends ERROR {
+export interface COMMENT_TO_REPLY_NOT_FOUND extends ERROR {
   type: 'business';
   result: false;
   code: 4012;
   data: '답글을 달 댓글을 찾지 못했어요.';
 }
 
-export interface ALREADY_REPORTED_ARTICLE extends ERROR {
+export interface ARTICLE_ALREADY_REPORTED extends ERROR {
   type: 'business';
   result: false;
   code: 4014;
-  data: '이미 신고한 게시글입니다.';
+  data: '이미 신고한 게시글이에요.';
 }
 
-export interface IS_NOT_WRITER_OF_THIS_ARTICLE extends ERROR {
+export interface NO_PERMISSION_TO_EDIT extends ERROR {
   type: 'business';
   result: false;
   code: 4015;
   data: '이 게시글의 작성자만이 수정할 수 있습니다.';
-}
-
-export interface CANNOT_FIND_ONE_COMMENT extends ERROR {
-  type: 'business';
-  result: false;
-  code: 4016;
-  data: '해당 댓글을 찾지 못했습니다.';
 }
