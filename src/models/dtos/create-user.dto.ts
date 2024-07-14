3056c44914100e2c826d9539837487112d27a119
YongChan Cho <h3236516@gmail.com>
2024-07-14 21:48:05 +0900

    refactor: create user dto

--- src/models/dtos/create-user.dto.ts
+++ src/models/dtos/create-user.dto.ts
@@ -1,13 +1,14 @@
-import { IsEmail, IsString, MaxLength } from 'class-validator';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @IsEmail({}, { message: '이메일 형식이 아닙니다.' })
+  @IsEmail()
   public email!: string;
 
-  @IsString({ message: '닉네임을 입력해주세요.' })
-  @MaxLength(21, { message: '닉네임은 21자 이하로 입력해주세요.' })
+  @IsString()
+  @MaxLength(21)
   public username!: string;
 
-  @IsString({ message: '비밀번호를 입력해주세요.' })
+  @IsString()
+  @MinLength(8)
   public password!: string;
 }
