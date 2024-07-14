1f7a2ac3d5f8a0c2d93f71a2384a7ed5ea761201
YongChan Cho <h3236516@gmail.com>
2024-07-14 21:47:29 +0900

    style: formatting

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -29,4 +29,4 @@ import { UsersService } from 'src/providers/users.service';
   providers: [AuthService, UsersService],
   exports: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }
