e91009c5f7e06a8e2eda047d1783f247321af5ea
YongChan Cho <h3236516@gmail.com>
2024-07-14 21:47:29 +0900

    style: formatting

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,11 +3,11 @@ import { AuthService } from './auth.service';
 import { UsersService } from 'src/providers/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { TypedBody, TypedRoute } from '@nestia/core';
-import { CreateUserDto } from 'src/models/dtos/create-user.dto';
 import { createResponseForm, TryCatch } from 'src/types';
 import { DecodedUserToken } from 'src/models/tables/user.entity';
 import { EMAIL_ALREADY_CREATED } from 'src/config/errors/error';
 import { isBusinessErrorGuard } from 'src/config/errors';
+import { CreateUserDto } from 'src/models/dtos/create-user.dto';
 
 @Controller('api/v1/auth')
 export class AuthController {
@@ -15,18 +15,17 @@ export class AuthController {
     private readonly authService: AuthService,
     private readonly jwtService: JwtService,
     private readonly usersService: UsersService,
-  ) {}
+  ) { }
 
   @TypedRoute.Post('signup')
   async signUp(
-    @TypedBody() createUserDto: CreateUserDto,
+    @TypedBody() createUserDto: CreateUserDto
   ): Promise<TryCatch<DecodedUserToken, EMAIL_ALREADY_CREATED>> {
     const createUserResponse = await this.usersService.create(createUserDto);
     if (isBusinessErrorGuard(createUserResponse)) {
       return createUserResponse;
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password, createdAt, ...user } = createUserResponse;
     return createResponseForm(user);
   }
