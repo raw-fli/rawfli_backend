3df934f475ea73273c30693ce34b6cc3c7062b36
YongChan Cho <h3236516@gmail.com>
2024-07-19 23:36:11 +0900

    feat: photo

--- src/providers/aws.service.ts
+++ src/providers/aws.service.ts
@@ -1,12 +1,19 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Photo } from "src/models/tables/photo.entity";
+import { DecodedUserToken } from "src/models/tables/user.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class AwsService {
   s3Client: S3Client;
 
-  constructor(private configService: ConfigService) {
+  constructor(
+    private configService: ConfigService,
+    @InjectRepository(Photo) private readonly photoRepository: Repository<Photo>,
+  ) {
     this.s3Client = new S3Client({
       region: this.configService.get('AWS_REGION'),
       credentials: {
@@ -16,7 +23,7 @@ export class AwsService {
     });
   }
 
-  async uploadImage(file: Express.Multer.File,) {
+  async uploadImage(user: DecodedUserToken, file: Express.Multer.File,) {
     const key = `${Date.now()}-${file.originalname}`;
     const command = new PutObjectCommand({
       Bucket: this.configService.get('AWS_S3_BUCKET'),
@@ -26,6 +33,7 @@ export class AwsService {
     });
 
     await this.s3Client.send(command);
-    return `https://s3.${this.configService.get('AWS_REGION')}.amazonaws.com/${this.configService.get('AWS_S3_BUCKET')}/${key}`;
+    const newPhoto = new Photo();
+    return await this.photoRepository.save(newPhoto);
   }
 }
