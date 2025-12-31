201c6f09f1bc73c6c536332483e3d15e41ade214
YongChan Cho <h3236516@gmail.com>
2025-12-31 15:57:21 +0900

    remove: remove nestia

--- src/controllers/aws.controller.ts
+++ src/controllers/aws.controller.ts
@@ -1,12 +1,12 @@
-import { TypedRoute } from "@nestia/core";
-import { Controller, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
+import { Controller, Post, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { ApiConsumes } from "@nestjs/swagger";
 import { JwtGuard } from "src/auth/guards/jwt.guard";
 import { UserDecorator } from "src/common/decorators/user.decorator";
+import { Photo } from "src/models/tables/photo.entity";
 import { DecodedUserToken } from "src/models/tables/user.entity";
 import { AwsService } from "src/providers/aws.service";
-import { createResponseForm } from "src/types";
+import { createResponseForm, Try } from "src/types";
 
 @Controller('api/v1/aws')
 export class AwsController {
@@ -14,12 +14,12 @@ export class AwsController {
 
   @UseGuards(JwtGuard)
   @ApiConsumes('multipart/form-data')
-  @TypedRoute.Post('upload')
+  @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(
     @UserDecorator() user: DecodedUserToken,
     @UploadedFile() image: Express.Multer.File,
-  ): Promise<string> {
+  ): Promise<Try<Photo>> {
     const photo = await this.awsService.uploadImage(user, image);
     return createResponseForm(photo);
   }
--- src/models/dtos/login-user.dto.ts
+++ src/models/dtos/login-user.dto.ts
@@ -1,6 +1,13 @@
-import { tags } from "typia";
+import { IsString } from "class-validator";
 
-export interface LoginUserDto {
-  email: string & tags.Format<"email">;
+export class LoginUserDto {
+  @IsString({
+    message: "이메일을 입력해주세요."
+  })
+  email: string;
+
+  @IsString({
+    message: "비밀번호를 입력해주세요."
+  })
   password: string;
 }
